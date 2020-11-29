fd430c4646b341be2441486d151ed33552cbc61e
Tejas Markande <markandetejasm@gmail.com>
2020-11-29 16:29:59 +0000

    Adding add functionality

--- adafruit.c
+++ adafruit.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <conio.h>
 
+/* Add function */
+void add (int a, int b)
+{
+  int c = 0;
+  c = a + b;
+  return;
+}
 #------------------------
 # Main Function template
 #-------------------------
 int main(void)
 {
+  int a=b=10;
+  add(a,b);
   return 0;
 }
